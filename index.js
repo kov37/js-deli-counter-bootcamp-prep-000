c3ca27c4f9d7d8e3d1f43f14cbdc798cd904cd69
kovner chalumo <kchalumo@gmail.com>
2018-12-26 01:35:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ function takeANumber(line, newPerson){
   return `Welcome, ${newPerson}. You are number ${line.length} in line.`;
 }
 
-function nowServing(line) {{
+function nowServing(line) {
   return line.length === 0 ? "There is nobody waiting to be served!" : `Currently serving ${line.shift()}.`;
-}}
+}
 
-function currentLine(line)
+function currentLine(line){
+  if (line.length === 0)
+    return "The line is currently empty."
+  var currentLine = ''
+  for (var i = 0; i < )
+}
