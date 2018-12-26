57c047cf74c28cd25e28b15ec724ebbad16edff9
kovner chalumo <kchalumo@gmail.com>
2018-12-26 01:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function takeANumber(line, newPerson){
   line.push(newPerson);
   return `Welcome, ${newPerson}. You are number ${line.length} in line.`;
 }
+
+function nowServing(line) {
+  return line.isEmpty() ? 
+}
