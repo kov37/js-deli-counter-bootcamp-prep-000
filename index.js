7697bc6667fc4570b91fe167f5ea25eece2debd1
kovner chalumo <kchalumo@gmail.com>
2018-12-26 01:41:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function nowServing(line) {
 function currentLine(line){
   if (line.length === 0)
     return "The line is currently empty."
-  var currentLine = ''
-  for (var i = 0; i < )
+  var currentLine = "The line is currently:"
+  for (var i = 0; i < line.length; i++){
+    currentLine += ` ${i+1}. ${line[i]}${i === line.length - 2 ? ',' : ''}`
+  }
+  return currentLine;
 }
