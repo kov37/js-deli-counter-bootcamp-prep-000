805629c1d63970f83d552d57aa8da31706add8ca
kovner chalumo <kchalumo@gmail.com>
2018-12-26 01:25:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(line, newPerson){
-  return "Welcome, ${newPerson}. You are number ${line.length+1} in line.";
+  return `Welcome, ${newPerson}. You are number ${line.length+1} in line.`;
   
 }
